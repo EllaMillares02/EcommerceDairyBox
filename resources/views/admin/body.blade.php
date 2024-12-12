e492855610a95b24ebe9a083d865343464c50544
EllaMillares02 <ellamaemillares20@gmail.com>
2024-12-12 22:29:55 +0800

    change color in near expiry

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -20,7 +20,7 @@
                 <div class="app-card app-card-stat shadow-sm h-100">
                     <div class="app-card-body p-3 p-lg-4">
                         <h4 class="stats-type mb-1">Products near expiry</h4>
-                        <div class="stats-figure">{{ $expiring_soon_count }}</div>
+                        <div class="stats-figure" style="color: red;">{{ $expiring_soon_count }}</div>
                     </div><!--//app-card-body-->
                     <a class="app-card-link-mask" href="#"></a>
                 </div><!--//app-card-->
