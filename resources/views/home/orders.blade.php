3b7350798b9925076a9927256358ac2b4b88d65f
EllaMillares02 <ellamaemillares20@gmail.com>
2024-12-12 22:35:35 +0800

    delete pagination

--- resources/views/home/orders.blade.php
+++ resources/views/home/orders.blade.php
@@ -208,19 +208,6 @@
                        
                     </div><!--//app-card-body-->		
                 </div><!--//app-card-->
-                <nav class="app-pagination">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item disabled">
-                            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                        </li>
-                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#">Next</a>
-                        </li>
-                    </ul>
-                </nav><!--//app-pagination-->
                 
             </div><!--//tab-pane-->
             
@@ -310,19 +297,6 @@
                            
                         </div><!--//app-card-body-->		
                     </div><!--//app-card-->
-                    <nav class="app-pagination">
-                        <ul class="pagination justify-content-center">
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">Next</a>
-                            </li>
-                        </ul>
-                    </nav><!--//app-pagination-->
                     
                 </div><!--//tab-pane-->
           
